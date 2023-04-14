d1a9e18fac32ea1a4a47af9aef660030e37898e5
Amberaj Singh <amberaj.singh@dotsquares.com>
2023-04-14 14:10:40 +0530

    remove some file commit

--- DotNetCoreWebApp/Controllers/HomeController.cs
+++ DotNetCoreWebApp/Controllers/HomeController.cs
@@ -29,11 +29,6 @@ namespace DotNetCoreWebApp.Controllers
 			
             return View();
         }
-		 public IActionResult Privacy1()
-        {
-			
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
